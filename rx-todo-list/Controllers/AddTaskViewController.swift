00ffe4827eb52469db493df5d209408e7c6fd7bb
Paul Defilippi <allinfo.pd@gmail.com>
2019-11-02 19:39:42 -0400

    designed add task screen

---
+++ rx-todo-list/Controllers/AddTaskViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AddTaskViewController.swift
+//  rx-todo-list
+//
+//  Created by Paul Defilippi on 11/2/19.
+//  Copyright © 2019 Paul Defilippi. All rights reserved.
+//
+
+import UIKit
+
+class AddTaskViewController: UIViewController {
+    
+}
